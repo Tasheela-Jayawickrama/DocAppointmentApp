224ed26f773674eae7956509962014849bae169b
Tashee <tasheelajayawickrama1999@gmail.com>
2022-11-24 06:51:39 +0530

    First Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Doctor Appointments</title>
+    @include('lib.styles')
+</head>
+<body>
+    @include('components.nav')
+
+    @yield('content')
+
+    @include('components.footer')
+
+    @include('lib.scripts')
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container" style="border: 2px solid red; height: 100vh">
+    <div class="row mx-auto">
+        <div class="col-lg-8 col-sm-12">
+            <div class="box" style="width: 100%; height: 500px; background-color: red;">1</div>
+        </div>
+        <div class="col-lg-4 col-sm-12">
+            <div class="box" style="width: 100p%; height: 500px; background-color: red;">1</div>
+        </div>
+    </div>
+
+</div>
+
+
+@endsection
+
