cbcdc317edaf5a825e0ed1a08a4a6f366b4d785b
Tashee <tasheelajayawickrama1999@gmail.com>
2022-11-24 06:54:06 +0530

    code added

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,6 +8,11 @@
         </div>
         <div class="col-lg-4 col-sm-12">
             <div class="box" style="width: 100p%; height: 500px; background-color: red;">1</div>
+
+        </div>
+        <div class="col-lg-4 col-sm-12">
+            <div class="box" style="width: 100p%; height: 500px; background-color: red;">1</div>
+
         </div>
     </div>
 
